0ed2774c516cb07b6212122f55b02531f6a888c9
Toby Goodwin <toby@helium.mv6.co.uk>
2014-12-13 19:59:25 +0000

    toggle and destroy now work

--- three.hs
+++ three.hs
@@ -1,55 +1,73 @@
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 
 module Main where
 
-import Prelude hiding ((++))
+import ClassyPrelude
 
-import ClassyPrelude ((++), readMay)
 import Control.Monad
 import Control.Monad.State (StateT, get, lift, runStateT, put)
-import Control.Monad.Trans.Control (control)
 import Data.Default
-import Data.IORef
-import qualified Data.List as L
+-- import Data.IORef
+import qualified Data.List as L hiding ((++))
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as LT
 import GHCJS.Foreign
-import JavaScript.JQuery
+import JavaScript.JQuery hiding (not)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import Text.Hamlet (shamlet)
 
 main = do
   let ts = initialTodos
   todoRef <- newIORef ts
-  l <- todoList todoRef
-  select "#todo-list-div" >>= appendJQuery l
-  myClick <- select "#destroy-3"
-  myCount <- select "<div>0</div>"
-  let getCount = atomicModifyIORef todoRef
-			(\c -> let c' = tail c
-                                in (c', T.pack $ show (length c')))
-      showCount _ = do
-        x <- getCount
-        setText x myCount
-        return ()
-  let destroy e = do
-	xs <- target e >>= selectElement >>= getAttr "n"
-	let mx = readMay xs
-	case mx of
-          Nothing -> return ()
-          Just n -> do
-            atomicModifyIORef todoRef $ todoDestroy n
-            select ("#todo-list-" ++ xs) >>= detach
-            updateBindings todoRef
-            return ()
-  buts <- select "button.destroy"
-  click destroy def buts
+  updateTodos todoRef
+--  myClick <- select "#destroy-3"
+--  myCount <- select "<div>0</div>"
+--  let getCount = atomicModifyIORef todoRef
+--			(\c -> let c' = L.tail c
+--                                in (c', tshow (length c')))
+--      showCount _ = do
+--        x <- getCount
+--        setText x myCount
+--        return ()
   -- select "body" >>= appendJQuery myCount
   --myThing <- fetchTodoList
   -- lift $ select "#todo-list-div" >>= appendJQuery myThing
   return ()
+
+updateTodos todoRef = do
+  l <- todoList todoRef
+  s <- select "#todo-list"
+  replaceWith l s
+  buts <- select "button.destroy"
+  click (destroy todoRef) def buts
+  togs <- select "input.toggle"
+  click (toggle todoRef) def togs
+
+destroy todoRef e = do
+  xs <- target e >>= selectElement >>= getAttr "n"
+  let mx = readMay xs
+  case mx of
+    Nothing -> return ()
+    Just n -> do
+      atomicModifyIORef todoRef $ todoDestroy n
+      select ("#todo-list-" ++ xs) >>= detach
+      updateBindings todoRef
+      return ()
+
+toggle todoRef e = do
+  xs <- target e >>= selectElement >>= getAttr "n"
+  let mx = readMay xs
+  case mx of
+    Nothing -> return ()
+    Just n -> do
+      atomicModifyIORef todoRef $ todoToggle n
+      -- target e >>= selectElement >>= toggle
+      updateTodos todoRef
+      updateBindings todoRef
+      return ()
 
 todoDestroy n ts =
   let mt = L.find (\(x,_,_) -> x == n) ts
@@ -57,42 +75,31 @@ todoDestroy n ts =
     Nothing -> (ts, ())
     Just t -> (L.delete t ts, ())
 
-thing2 :: Int -> StateT Todos IO ()
-thing2 i = do
-  ts <- get
-  let mt = L.find (\(x,_,_) -> x == i) ts
-  case mt of
-    Nothing -> do
-      myThing <- lift $ select "<div>not found</div>"
-      lift $ select "body" >>= appendJQuery myThing
-      return ()
-    Just t -> do
-      put $ L.delete t ts
-      lift $ select (T.append "#todo-list-" (T.pack (show i))) >>= detach
-      -- updateBindings
-      return ()
+todoToggle n ts =
+  let mt = L.find (\(x,_,_) -> x == n) ts
+  in case mt of
+    Nothing -> (ts, ())
+    Just todo@(i, t, c) -> ((i, t, not c) : L.delete todo ts, ())
 
 updateBindings r = do
   ts <- readIORef r
-  myThing <- select $ "<div>" ++ (T.pack $ show ts) ++ "</div>"
+  myThing <- select $ "<div>" ++ tshow ts ++ "</div>"
   select "body" >>= appendJQuery myThing
   let nDone = L.length $ L.filter (\(_, _, c) -> c) ts
       nLeft = L.length ts - nDone
-  select "#bind-n-left" >>= setText (T.pack $ show nLeft)
-  select "#bind-n-done" >>= setText (T.pack $ show nDone)
-  -- e <- lift $ select "#todo-count"
-  -- setText (T.pack $ show nLeft) e
+  select "#bind-n-left" >>= setText (tshow nLeft)
+  select "#bind-n-done" >>= setText (tshow nDone)
   
 todoList r = do
   ts <- readIORef r
-  select $ T.concat $ LT.toChunks $ renderHtml [shamlet|$newline always
+  return $ T.concat $ LT.toChunks $ renderHtml [shamlet|$newline always
     <ul #todo-list>
       $forall (i, t, c) <- ts
         <li :c:.completed #todo-list-#{i}>
-          <input .toggle type=checkbox>
+          <input .toggle n=#{i} type=checkbox>
           <label>
             #{t}
-          <button #destroy-#{i} n=#{i} class=destroy>
+          <button .destroy n=#{i}>
   |]
 
 type Todo = (Int, Text, Bool)
